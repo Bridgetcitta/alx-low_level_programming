7af3b8c49a90e5bdca301513bed60d31093fdb48
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-19 16:47:07 -0700

    Updated files: 0x03-debugging/README.md 0x03-debugging/Xgit 0x03-debugging/git 0x03-debugging/main.c 0x03-debugging/main.h 0x00-hello_world/0x01-variables_if_else_while README.md

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - text function for positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
