0a7f24ec9b2b5dca7d2ab2903eb63baac6f2176e
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-20 16:42:14 -0700

    Updated files: 0x04-more_functions_nested_loops/5-more_numbers.c 0x04-more_functions_nested_loops/main.h 0x00-hello_world/0x01-variables_if_else_while

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - 10 times the numbers, from 0 to 14,
+ * followed by a new line
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+
+	int i, n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n >= 10)
+				_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+
+}
