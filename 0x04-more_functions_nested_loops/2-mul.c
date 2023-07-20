b232b6beb6632f7ede3a5a53a59eb13b6540fe39
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-20 14:29:42 -0700

    Updated files: 0x04-more_functions_nested_loops/2-mul.c 0x04-more_functions_nested_loops/main.h 0x00-hello_world/0x01-variables_if_else_while

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul -  multiplies two integers
+ * @a: firt integer
+ * @b: second integer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int a = 2;
+	int b = 3;
+
+	_putchar("%d\n", 2, 3, mul(a, b));
+
+	return (0);
+}
