aa798f76d1f6cdd833a8aaa0800b9a1557798a80
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-20 19:10:59 -0700

    Updated files: 0x04-more_functions_nested_loops/8-print_square.c 0x00-hello_world/0x01-variables_if_else_while

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,7 @@ void print_square(int size)
 	{
 		_putchar('\n');
 	}
+
 	else
 	{
 		int i, j;
@@ -18,11 +19,9 @@ void print_square(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-			{
 				_putchar('#');
-			}
-
-			_putchar('\n');
 		}
+
+		_putchar('\n');
 	}
 }
