e2761f106dae1f566275387799a5b980f8aec672
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-24 22:01:13 -0700

    Updated files: 0x05-pointers_arrays_strings/7-puts_half.c 0x00-hello_world/0x01-variables_if_else_while

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,16 +10,16 @@ void puts_half(char *str)
 {
 	int i, n;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; ++i)
+		;
 
 	if (i % 2 == 0)
 	{
-		for (n = i / 2; str[n] != '\0'; n++)
+		for (n = i / 2; str[n] != '\0'; ++n)
 			_putchar(str[n]);
-	}
-	else
+	} else
 	{
-		for (n = ((i - 1) / 2) + 1; str[n] != '\0'; n++)
+		for (n = ((i - 1) / 2) + 1; str[n] != '\0'; ++n)
 			_putchar(str[n]);
 	}
 	_putchar('\n');
