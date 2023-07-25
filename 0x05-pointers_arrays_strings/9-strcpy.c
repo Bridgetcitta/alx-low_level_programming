b7845d429bf123a52fe464617a012aa0fc20a034
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-24 22:56:53 -0700

    Updated files: 0x05-pointers_arrays_strings/9-strcpy.c 0x00-hello_world/0x01-variables_if_else_while

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,15 +11,15 @@
 
 char *_strcpy(char *dest, char *src)
 {
+	int a, b = 0;
 
-	while (*src != '\0')
+	for (a = 0; src[a] != '\0'; a++)
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[b] = src[a];
+		b++;
 	}
 
-	*dest = '\0';
+	dest[b] = '\0';
 
 	return (dest);
 }
