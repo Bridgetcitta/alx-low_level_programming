adf6a3a9baa50be809d2baf075d5fee283fcac93
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-24 19:29:27 -0700

    Updated files: 0x05-pointers_arrays_strings/6-puts2.c 0x00-hello_world/0x01-variables_if_else_while

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts2 - prints every other character of a string,
@@ -8,12 +9,11 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len = strlen(str);
 
-	while (str[i] != '\0')
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 
 	_putchar('\n');
