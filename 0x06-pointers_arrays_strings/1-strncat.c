e3049b05407847c6432c56b7c77b228337fb31ac
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-27 14:39:54 -0700

    Updated files: 0x06-pointers_arrays_strings/1-strncat.c 0x00-hello_world/0x01-variables_if_else_while

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * @n: bytes from src
+ * @src: source string
+ * @dest: destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, a;
+
+	i = 0;
+
+	while (dest[i])
+		i++;
+
+	for (a = 0; a < n &&  src[a] != '\0'; a++)
+		dest[i + a] = src[a];
+	dest[i + a] = '\0';
+
+	return (dest);
+}
