734dbd386a0a8817fd82f01ab48a93ae78c3df98
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-27 13:53:09 -0700

    Updated files: 0x06-pointers_arrays_strings/0-strcat.c 0x06-pointers_arrays_strings/README.md 0x06-pointers_arrays_strings/Xgit 0x06-pointers_arrays_strings/git 0x06-pointers_arrays_strings/main.h 0x00-hello_world/0x01-variables_if_else_while

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i])
+		i++;
+	for (j = 0; src[j]; j++)
+		dest[i++] = src[j];
+	return (dest);
+}
