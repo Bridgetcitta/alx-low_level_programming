39d313509e74a857a99f22ca7a331cee0eb1437b
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-27 20:01:31 -0700

    Updated files: 0x06-pointers_arrays_strings/101-print_number.c 0x00-hello_world/0x01-variables_if_else_while

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,20 @@
 
 void print_number(int n)
 {
+	unsigned int a;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
+	a = n;
+
+	if (a / 10)
 	{
-		print_number(n / 10);
+		print_number(a / 10);
 	}
 
-	_putchar(n % 10 + 0);
+	_putchar(a % 10 + '0');
 }
