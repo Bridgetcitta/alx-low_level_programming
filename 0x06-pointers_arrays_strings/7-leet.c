65d3de9c4934e459fa474ebc3dc967ee2bea025c
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-27 18:17:29 -0700

    Updated files: 0x06-pointers_arrays_strings/7-leet.c 0x00-hello_world/0x01-variables_if_else_while

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet -  encodes a string into 1337
+ * @s: parameter input
+ * Return: Always 0
+ */
+
+char *leet(char *s)
+{
+	char *leet_chars = "AEGOTLaegotl";
+	char *leet_nums = "43671";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; leet_chars[j] != '\0'; j++)
+
+			if (s[i] == leet_chars[j])
+			{
+				s[i] = leet_nums[j / 2];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
