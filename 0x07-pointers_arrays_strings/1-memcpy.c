659d0d163b0ba644e8033f57cfa54cd880a79a2f
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-07-31 16:48:00 -0700

    Updated files: 0x07-pointers_arrays_strings/1-memcpy.c 0x00-hello_world/0x01-variables_if_else_while Xgit git

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @n: number of bytes to copy
+ * @src: char source
+ * @dest: char destination
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+}
