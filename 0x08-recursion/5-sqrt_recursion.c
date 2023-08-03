c6bc16e2da373a0bb9a2b697057c212495923129
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-02 22:02:02 -0700

    Updated files: 0x08-recursion/5-sqrt_recursion.c 0x08-recursion/main.h

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,26 +9,35 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
+
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
 	return (sqrt_helper(n, 0, n));
 }
 
 /**
  * sqrt_helper - Recursive helper function for _sqrt_recursion.
  * @n: The number to find the square root of.
- * @low: The lowest possible square root of n.
- * @high: The highest possible square root of n.
+ * @start: The lowest possible square root of n.
+ * @end: The highest possible square root of n.
  *
  * Return: The square root of n, -1 if n does not have a natural square root.
  */
-int sqrt_helper(int n, int low, int high)
+
+int sqrt_helper(int n, int start, int end)
 {
-	int mid = (low + high) / 2;
+	int mid = (start + end) / 2;
 
 	if (mid * mid == n)
 	{
 		return (mid);
 	}
+
 	if (mid * mid < n)
 	{
 		if ((mid + 1) * (mid + 1) > n)
@@ -37,11 +46,12 @@ int sqrt_helper(int n, int low, int high)
 		}
 		else
 		{
-			return (sqrt_helper(n, mid + 1, high));
-		}
 
-	} else
+			return (sqrt_helper(n, mid + 1, end));
+		}
+	}
+	else
 	{
-		return (sqrt_helper(n, low, mid - 1));
+		return (sqrt_helper(n, start, mid - 1));
 	}
 }
