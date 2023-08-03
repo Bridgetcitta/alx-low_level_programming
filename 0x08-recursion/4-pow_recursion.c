693b2b68e8a8a0899b800ffbb1468bf06a70927e
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-02 19:19:42 -0700

    Updated files: 0x08-recursion/4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,13 +9,20 @@
 
 int _pow_recursion(int x, int y)
 {
-	int i;
 
 	if (y < 0)
 	{
 		return (-1);
 	}
 
-	i = _pow_recursion(x, y);
-	return (i);
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+
+	return (x * _pow_recursion(x, y - 1));
+	}
 }
