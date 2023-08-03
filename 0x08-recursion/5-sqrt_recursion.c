7bde045a88bd8c1ae193e41f57613677ae33a57a
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-02 20:27:54 -0700

    Updated files: 0x08-recursion/5-sqrt_recursion.c 0x08-recursion/main.h

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,28 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
+	return (sqrt_helper(n, 0, n));
+}
 
-	_sqrt_recursion(n);
+/**
+ * sqrt_helper - Recursive helper function for _sqrt_recursion.
+ * @n: The number to find the square root of.
+ * @low: The lowest possible square root of n.
+ * @high: The highest possible square root of n.
+ *
+ * Return: The square root of n, -1 if n does not have a natural square root.
+ */
+int sqrt_helper(int n, int low, int high)
+{
+	int mid = (low + high) / 2;
 
-	return (0);
+	if (mid * mid == n)
+		return (mid);
+	if (low >= high)
+		return (-1);
+	if (mid * mid > n)
+
+		return (sqrt_helper(n, low, mid - 1));
+	return (sqrt_helper(n, mid + 1, high));
 }
