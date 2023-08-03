f1af60c87a376536cc5e6ae682b088fdb3ff955d
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-02 19:30:43 -0700

    Updated files: 0x08-recursion/5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: -1 If n does not have a natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+	else if (n > 0)
+		return (1);
+	else
+		return (_sqrt_recursion(n - 1));
+}
