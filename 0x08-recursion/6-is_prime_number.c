13f113d255f6dd015fcad25c164102c1a99eef73
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-02 21:07:48 -0700

    Updated files: 0x08-recursion/6-is_prime_number.c 0x08-recursion/main.h

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,33 @@
 
 int is_prime_number(int n)
 {
-	int i;
 
-	if (n < 2)
+	if (n <= 1)
+	{
 		return (0);
-	for (i = 2; i <= n / 2; i++)
-		if (n % i == 0)
-			return (0);
-	return (1);
+	}
+
+	return (is_prime_helper(n, 2));
+}
+
+/**
+ * is_prime_helper - is a helper function
+ * @n: integer input
+ * @i: integer input
+ * Return:1 if the input integer is a prime number, otherwise return 0.
+ */
+
+int is_prime_helper(int n, int i)
+{
+
+	if (i == n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+
+	return (is_prime_helper(n, i + 1));
 }
