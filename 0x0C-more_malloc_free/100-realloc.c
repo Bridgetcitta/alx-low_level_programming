e21ba1aa66092686fefad8464b33f274490e752e
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-11 21:51:25 -0700

    Updated files: 0x0C-more_malloc_free/100-realloc.c 0x0C-more_malloc_free/vi

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,30 +10,41 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int copy_size;
+	unsigned int i;
 	char *new_ptr;
+	char *old_ptr;
 
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
+	if (new_size == 0 && ptr)
 	{
+
+		free(ptr);
 		return (NULL);
 	}
-	copy_size = old_size < new_size ? old_size : new_size;
-	memcpy(new_ptr, ptr, copy_size);
-	free(ptr);
 
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			new_ptr[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = old_ptr[i];
+	}
+	free(ptr);
 	return (new_ptr);
 }
