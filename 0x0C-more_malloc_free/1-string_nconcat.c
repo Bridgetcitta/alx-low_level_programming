ded149ea8df481d2aba56850c6365c066bd500e3
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-11 19:19:31 -0700

    Updated files: 0x0C-more_malloc_free/1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len2 = strlen(s2);
 	char *s;
 
-	len1 = 0;
-	len2 = 0;
+	while (s1 && s1[len1])
+		len1++;
+
+	while (s2 && s2[len2])
+		len2++;
 
 	if (n >= len2)
 		n = len2;
