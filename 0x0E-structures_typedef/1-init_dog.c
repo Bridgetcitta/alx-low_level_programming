c6b4385dee27a73169e79535ad1253a2d90a55ef
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-14 21:03:52 -0700

    Updated files: 0x0E-structures_typedef/1-init_dog.c 0x0E-structures_typedef/dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,6 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog -  initialize a variable of type struct dog
+ *
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
-#ifndef DOG_STRUCT_H
-#define DOG_STRUCT_H
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 
 struct dog
 {
@@ -8,4 +14,17 @@ struct dog
 	float age;
 };
 
-#endif /* DOG_STRUCT_H */
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
+
