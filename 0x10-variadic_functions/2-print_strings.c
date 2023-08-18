93ed5db3a9f0c0e69c93031190c373a4ee5eccbb
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-17 19:56:14 -0700

    Updated files: 0x10-variadic_functions/2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,9 +20,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	printf("%d", str);
 
 	if (i != n - 1 && separator != NULL)
+	{
 		printf("%s", separator);
-	if ((i = n - 1) || (separator == NULL)
+	}
+	if ((i == n - 1) || (separator == NULL))
+	{
 		printf("%s", "(nil)");
+	}
+
+	va_end(args);
 
 	printf("\n");
 }
