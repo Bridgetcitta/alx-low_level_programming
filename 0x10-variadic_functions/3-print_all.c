1c3bb75993f462be52a38c2dc273c70b736ec32a
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-17 21:59:24 -0700

    Updated files: 0x10-variadic_functions/3-print_all.c "0x10-variadic_functions/\\"

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: is a list of types of arguments passed to the function
+ * Return:0
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	const char *print = format;
+	char c;
+	int i;
+	double d;
+	char *s;
+
+	va_start(args, format);
+	while (*print)
+	{
+		if (*print == 'c')
+			c = va_arg(args, int);
+			printf("%c", c);
+		else if (*print == 'i')
+			i = va_arg(args, int);
+			printf("%d", i);
+		else if (*print == 'f')
+			d = va_arg(args, double);
+			printf("%f", d);
+		else if (*print == 's')
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("(nil");
+			else
+				printf("%s", s);
+			print++;
+	}
+	va_end(args);
+	printf("\n");
+}
