8d137a74833dfcd43dc8415e069ba4d5e6b61526
Bridgetcitta <oforibridget20@gmail.comclear% sudo su -%sudo su -sudosudo -i>
2023-08-17 19:46:04 -0700

    Updated files: 0x10-variadic_functions/2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @n: number of strings passed
+ * @separator: string to be printed between the strings
+ * Return: 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		str = va_arg(args, int);
+	printf("%d", str);
+
+	if (i != n - 1 && separator != NULL)
+		printf("%s", separator);
+	if ((i = n - 1) || (separator == NULL)
+		printf("%s", "(nil)");
+
+	printf("\n");
+}
