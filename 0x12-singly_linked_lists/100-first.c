ad5d8196913c4d1fe44cec39cb97ee248426caa9
Bridgetcitta <oforibridget20@gmail.com>
2023-08-25 18:16:37 -0700

    Updated files: 0x12-singly_linked_lists/100-first.c 0x12-singly_linked_lists/first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,7 @@ void print_message(void)
 {
 	printf("you're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+
 }
 
 /**
@@ -20,5 +21,9 @@ void print_message(void)
 int main(void)
 {
 	print_message();
+
+	printf("A tortoise, having pretty good sense of a hare's nature,\n");
+	printf("challenges one to a race.\n");
+
 	return (0);
 }
