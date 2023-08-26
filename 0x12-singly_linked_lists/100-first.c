218c0804c183928431cfbce006d5ee16959ca28c
Bridgetcitta <oforibridget20@gmail.com>
2023-08-25 17:49:37 -0700

    Updated files: 0x12-singly_linked_lists/100-first.c 0x12-singly_linked_lists/first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * print_message - prints message
+ * Return: 0
+ */
+
+void print_message(void)
+{
+	printf("you're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_message();
+	return (0);
+}
