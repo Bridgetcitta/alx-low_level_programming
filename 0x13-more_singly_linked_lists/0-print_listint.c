f5388fb165972ce7bf43995dd2e434a5bea464d0
Bridgetcitta <oforibridget20@gmail.com>
2023-08-29 17:08:01 -0700

    Updated files: 0x13-more_singly_linked_lists/0-print_listint.c README.md

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t node = 0;
 
-	if (h == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
