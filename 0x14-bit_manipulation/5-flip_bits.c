0fca6d171d03dd3a93fd48609bea5752e0eb33e1
Bridgetcitta <oforibridget20@gmail.com>
2023-08-31 18:58:56 -0700

    Updated files: 0x14-bit_manipulation/5-flip_bits.c README.md

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to flip
+ * @n: integer input
+ * @m: parameter member
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result;
+	unsigned int num;
+
+	result = n ^ m;
+	num = 0;
+
+	while (result)
+	{
+		num++;
+		result &= result - 1;
+	}
+	return (num);
+}
