c3e6cca76cf1318f8a34e8b45c5f90c1e2db4844
Bridgetcitta <oforibridget20@gmail.com>
2023-09-05 00:19:34 -0700

    Updated files: 0x15-file_io/100-elf_header.c 0x15-file_io/elf_header --

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,86 +1,74 @@
 #include "main.h"
 #include <elf.h>
-void print_error(const char *message)
+/**
+ * display_error - prints errors
+ * @message: parameter member
+ * Return: 0
+ */
+void display_error(const char *message)
 {
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
 
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
+/**
+ * display_elf_header_info - prints nums on elf header.
+ * @elf_header: elf header input
+ * Return: 0
+ */
+void display_elf_header_info(const Elf64_Ehdr *elf_header)
+{
+	printf("Magic: %02x %02x %02x %02x\n",
+			elf_header->e_ident[EI_MAG0],
+			elf_header->e_ident[EI_MAG1],
+			elf_header->e_ident[EI_MAG2],
+			elf_header->e_ident[EI_MAG3]);
+	printf("Class: %d\n", elf_header->e_ident[EI_CLASS]);
+	printf("Data: %d\n", elf_header->e_ident[EI_DATA]);
+	printf("Version: %d\n", elf_header->e_ident[EI_VERSION]);
+	printf("OS/ABI: %d\n", elf_header->e_ident[EI_OSABI]);
+	printf("ABI Version: %d\n", elf_header->e_ident[EI_ABIVERSION]);
+	printf("Type: %d\n", elf_header->e_type);
+	printf("Entry point address: %lx\n", elf_header->e_entry);
+
 }
 /**
- * print_error - prints errors
- * *@message: paramter mem
  * main - entry
- * @argv: pointer intput
- * @argc: parmeter memb
- * Return: 0
+ * @argc: num of arguments
+ * @argv: an array of pointer to argument
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
-	const char *filename;
+	const char *filename = argv[1];
 	int fd;
 	Elf64_Ehdr elf_header;
 
 	if (argc != 2)
 	{
-		print_error("Usage: elf_header elf_filename");
-		return (98);
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename");
 	}
-	filename = argv[1];
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
-		print_error("Could not open the file");
-		return (98);
+		dprintf(STDERR_FILENO, "Error: failed to open the file");
 	}
-/**
- * close_elf - Closes an ELF file.
- * @elf: The file descriptor of the ELF file.
- */
-void close_elf(int elf)
-{
-	if (close(elf) == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", elf);
-		exit(98);
-	}
-}
-/**
- * main - Displays the information contained in the
- * ELF header at the start of an ELF file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- * Return: 0 on success.
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
-{
 	if (read(fd, &elf_header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 	{
-		print_error("Could not read ELF header");
-		close(fd);
-		return (98);
+
+		dprintf(STDERR_FILENO, "Failed to read ELF header");
 	}
-	close(fd);
+
 	if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
-			elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
-			elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
-			elf_header.e_ident[EI_MAG3] != ELFMAG3)
+		elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
+		elf_header.e_ident[EI_MAG2] != ELFMAG2 ||
+		elf_header.e_ident[EI_MAG3] != ELFMAG3)
 	{
-		print_error("Not an ELF file");
-		return (98);
+		dprintf(STDERR_FILENO, "Not an ELF file");
 	}
-	printf("Magic: %c%c%c%c\n", elf_header.e_ident[EI_MAG0], 
-			elf_header.e_ident[EI_MAG1], elf_header.e_ident[EI_MAG2], 
-			elf_header.e_ident[EI_MAG3]);
-	printf("Class: %d-bit\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? 64 : 32);
-	printf("Data: %s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ?
-			"2's complement, little endian" : "unknown");
-	printf("Version: %d (current)\n", elf_header.e_ident[EI_VERSION]);
-	printf("OS/ABI: %s\n", elf_header.e_ident[EI_OSABI] == ELFOSABI_SYSV
-			? "UNIX - System V" : "Other");
-	printf("ABI Version: %d\n", elf_header.e_ident[EI_ABIVERSION]);
-	printf("Type: %s\n", elf_header.e_type == ET_EXEC ? "EXEC (Executable file)" :
-			elf_header.e_type == ET_DYN ? "DYN (Shared object file)" :
-			elf_header.e_type == ET_REL ? "REL (Relocatable file)" : "unknown");
-	printf("Entry point address: 0x%lx\n", (unsigned long)elf_header.e_entry);
+
+	display_elf_header_info(&elf_header);
+	close(fd);
 	return (0);
 }
