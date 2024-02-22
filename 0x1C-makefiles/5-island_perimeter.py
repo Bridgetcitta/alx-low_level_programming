092511ad2d03e4f6bd94b5eabdc4759d040bb10a
Bridgetcitta <oforibridget20@gmail.com>
2024-02-21 17:49:33 -0800

    Updated files: 0x1C-makefiles/5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-    """A function that returns the perimeter of the island
-        described in grid
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Parameters:
+    - grid (list of list of integers): Represents the island grid.
+
+    Returns:
+    - int: Perimeter of the island.
     """
 
-    perimeter = 0
-    if grid is None or grid[0] is None:
+    if not grid or not grid[0]:
         return 0
-    else:
-        adjacents = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 0:
-                    continue
-                origin = 4
-                for adj in adjacents:
-                    x_axis = i + adj[0]
-                    y_axis = j + adj[1]
-                    if 0 < x_axis < len(grid) and 0 < y_axis < len(grid[i]) and\
-                       grid[x_axis][y_axis] == 1:
-                        origin -= 1
-                perimeter += origin
 
-        return perimeter
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Each land cell contributes 4 sides
+
+                # Check adjacent cells and subtract 1 for each shared side
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
